1ec0467f023c75326222dd17380605b39f56b4ad
Maver1ck1111 <borisovd885@gmail.com>
2025-09-16 23:24:42 +0300

    added cors policy

--- HTMLTemplateGenerator.WebApi/Program.cs
+++ HTMLTemplateGenerator.WebApi/Program.cs
@@ -11,10 +11,23 @@ builder.Host.UseSerilog((context, configuration) =>
     configuration.ReadFrom.Configuration(context.Configuration);
 });
 
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+
+    });
+});
+
 builder.Services.ApplicationDependencies();
 builder.Services.InfrastructureDependencies();
 
 var app = builder.Build();
+
+app.UseCors("AllowFrontend");
 
 app.MapControllers();
 
