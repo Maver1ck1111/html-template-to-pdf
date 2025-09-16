4d2b1430e16e53d6f697d196121c2f2f3e83d204
Maver1ck1111 <borisovd885@gmail.com>
2025-09-16 14:31:32 +0300

    added clean architecture pattern

---
+++ HTMLTemlateGenerator.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLTemlateGenerator.Infrastructure
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection InfrastructureDependencies(this IServiceCollection service)
+        {
+            return service;
+        }
+    }
+}
--- HTMLTemplateGenerator.WebApi/Program.cs
+++ HTMLTemplateGenerator.WebApi/Program.cs
@@ -1,6 +1,15 @@
+using HTMLTemlateGenerator.Infrastructure;
+using HTMLTemplateGenerator.Application;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.ApplicationDependencies();
+builder.Services.InfrastructureDependencies();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapControllers();
 
 app.Run();
